7a6bdb19ddf4b5297616f173186b721dc83c3bc4
Diego Cardoso <diegocar448@hotmail.com>
2021-08-18 14:39:47 -0300

    listview

---
+++ src/main/java/br/com/globallabs/springwebmvc/model/Jedi.java
@@ -0,0 +1,31 @@
+package br.com.globallabs.springwebmvc.model;
+
+public class Jedi {
+    private String name;
+    private String lastName;
+
+    public Jedi(final String name, final String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Jedi() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return name;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+}
